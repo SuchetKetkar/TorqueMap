65f91c0971514b218d2fd0e33a75a536d93d55ab
SuchetKetkar <suchet.ketkar@gmail.com>
2020-08-20 15:57:51 -0500

    adding testing code first

---
+++ torquemap.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import mpl_toolkits
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+u = np.linspace(0, 2*np.pi, 100)
+v = np.linspace(0, np.pi, 100)
+
+x = 10 * np.outer(np.cos(u), np.sin(v))
+y = 10 * np.outer(np.sin(u), np.sin(v))
+z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
+ax.plot_surface(x, y, z, rstride = 4, cstride = 4, color = 'b')
+
+plt.show()
