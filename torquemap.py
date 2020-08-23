ac68cff3ae08564b0d9c88151be65bd78b50cf58
SuchetKetkar <suchet.ketkar@gmail.com>
2020-08-22 20:25:45 -0500

    pivoting from surface to multi-plotted lines for wireframe type system

--- torquemap.py
+++ torquemap.py
@@ -22,41 +22,79 @@ z = []
 
 reader = csv.reader(Tmap, delimiter = ",")
 line = 0
-for row in reader:
-    print(row)
-    print("\n")
-    if line == 0:
-        for i in range(len(row)):
-            try:
-                x.append(float(row[i]))
-            except:
-                row.remove('')
-    else:
-        for i in range(len(row)):
-            row[i] = float(row[i])
-            if i == 0:
-                y.append(row[i])
-            else:
-                z.append(row[i])
-    line+=1
-print("x:    " + str(x))
-print("\n")
-print("y     " + str(y))
-print("\n")
-print("z     " + str(z))
+xlabels = next(reader)
+
+for i in range(len(xlabels)):
+    try:
+        x.append(float(xlabels[i]))
+    except:
+        xlabels.remove('')
+#xlabels.remove(0.0)
+
+row2 = next(reader)
+for i in range(len(row2)):
+    try:
+        z.append(float(row2[i]))
+    except:
+        row.remove('')
+
+y.extend(z[0] for i in range(len(z)-1))
+
+z.remove(z[0])
+
+#z = row2
+
+x = np.array(x)
+y = np.array(y)
+z = np.array(z)
 
 
+print(len(x))
+print(len(y))
+print(len(z))
+#for row in reader:
+    #print(row)
+    #print("\n")
+    #if line == 0:
+        #for i in range(len(row)):
+            #try:
+                #x.append(float(row[i]))
+            #except:
+                #row.remove('')
+    #else:
+        #for i in range(len(row)):
+            #row[i] = float(row[i])
+            #if i == 0:
+                #y.append(row[i])
+            #else:
+                #z.append(row[i])
+    #line+=1
+
+#print(x)
+#print(y)
+#if len(y) != len(x):
+    #y = np.linspace(y[0], y[-1], num = len(x))
 
 
-#fig = plt.figure()
-#ax = fig.gca(projection='3d')
+#plt.xticks(y,y)
+#plt.xticks(z,z)
 
+#x = np.array(x)
+#y = np.array(y)
+#z = np.array(z)
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+plt.xticks(x,x)
 #u = np.linspace(0, 2*np.pi, 100)
 #v = np.linspace(0, np.pi, 100)
 
 #x = 10 * np.outer(np.cos(u), np.sin(v))
 #y = 10 * np.outer(np.sin(u), np.sin(v))
 #z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
-#ax.plot_surface(x, y, z, rstride = 4, cstride = 4, color = 'b')
-
-#plt.show()
+ax.scatter3D(y, x, z, linestyle  = 'solid', color = 'blue')
+ax.plot(y, x, z, linestyle  = 'solid', color = 'blue')
+#ax.plot3D(y,x,z)
+plt.show()
